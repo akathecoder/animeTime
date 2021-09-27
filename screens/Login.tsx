dc09b967fcc37b90751c1e13927084af58f088fc
Sparsh Agarwal <sparshaga25@gmail.com>
2021-09-27 11:21:48 +0530

    Implemented Deep Linking and Web Browser

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {
     Box,
@@ -7,11 +7,33 @@ import {
     Heading,
     Icon,
     Image,
+    Text,
 } from 'native-base';
 import { logo } from '../utilities/images';
 import { MaterialIcons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import * as Linking from 'expo-linking';
 
 const Login = () => {
+    const [result, setResult] = useState<string>('');
+
+    const _openAuthSessionAsync = async () => {
+        try {
+            let requiredResult =
+                await WebBrowser.openAuthSessionAsync(
+                    `https://backend-xxswjknyfi.now.sh/?linkingUri=${Linking.createURL(
+                        '/?',
+                    )}`,
+                    '',
+                );
+
+            setResult(JSON.stringify(requiredResult));
+        } catch (error) {
+            alert(error);
+            console.log(error);
+        }
+    };
+
     return (
         <Box
             flex={1}
@@ -20,7 +42,6 @@ const Login = () => {
             py={3}
             bgColor="lightBlue.500"
         >
-            {/* <ScrollView flex={1}> */}
             <Center width="100%" height="100%">
                 <Image source={logo} size="xl" alt="Logo" />
 
@@ -44,11 +65,13 @@ const Login = () => {
                     size="lg"
                     mt={12}
                     colorScheme="light"
+                    onPress={_openAuthSessionAsync}
                 >
                     Login with AniList
                 </Button>
+
+                <Text mt={2}>{result}</Text>
             </Center>
-            {/* </ScrollView> */}
 
             <StatusBar style="auto" />
         </Box>
